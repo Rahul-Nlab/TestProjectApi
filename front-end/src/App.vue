9c7bfed3b6fb6cea8d29d2d30faf249440f4f5e9
Rahul <rahul.somaliya@nirmanlab.com>
2022-02-10 18:41:23 +0530

    Frontend Improved Some UI changes, Edit functionality added New button functionality added

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -13,59 +13,103 @@
 
           <div class = "tableFirstName"> <b>First Name</b> </div>
 
+          <div class = "tableMiddleName"> <b>Middle Name</b> </div>
+
+          <div class = "tableLastName"> <b>Last Name</b> </div>
+
           <div class = "tableActions"> <b> Actions </b> </div>
 
         </div>
 
-          <div class = "userTable" v-for="i in newVar" :key = i.u_id >
+        <div class = "userTable" v-for="i in newVar" :key = i.u_id >
 
-            <div class="tableId">
+          <div class="tableId">
 
-              {{i.u_id}}
-
-            </div>
-
-            <div class="tableFirstName">
-
-              {{i.first_name}}
-
-            </div>
-
-            <div class="tableActions">
-
-              <button> Show more </button>
-                    &emsp;
-              <button> Edit </button>
-                    &emsp;
-              <button v-on:click="deleteUserId(i.u_id), setShow()"> Delete User </button>
-
-            </div>
+            {{i.u_id}}
 
           </div>
 
-          <div >
+          <div class="tableFirstName">
 
-            <button @click="newUser(id)"> New User </button>
-    <!-- HERE I HAVE TO CREATE USER DETAIL FORM THAT POPS UP ONLY WHEN CREATE USER IS CLICKED, AND DISPLAYS A FORM THAT ACCEPTS DATA AND CALLS A FUNCTION ON SUBMIT CLICK WITH A JSON FORMAT PARAMETER -->
-    <!-- ADD A SWITCH THAT SHOWS EITHER THE FORM OR THE NEW USER BUTTON -->
-            <div class = "formStyle">
-
-                <form>
-                  Enter Details: <br>
-
-                  <input v-model="userId" type="text" id="id" name="id" placeholder='User Id'><br>
-                  <input v-model="firstName" type="text" id="fname" name="fname" placeholder='First Name'><br>
-                  <input v-model="middleName" type="text" id="mname" name="mname" placeholder='Middle Name'><br>
-                  <input v-model="lastName" type="text" id="lname" name="lname" placeholder='Last name'>
-
-                  <button @click="newUser()"> Create </button>
-
-                </form>
-
-            </div>
+            {{i.first_name}}
 
           </div>
 
+
+          <div class="tableMiddleName">
+
+            {{i.middle_name}}
+
+          </div>
+
+          <div class="tableLastName">
+
+            {{i.last_name}}
+
+          </div>
+
+          <div class="tableActions">
+
+            <button> Show Address </button>
+                  &emsp;
+            <button v-on:click="setEditValues(i), showEditForm()"> Edit </button>
+            <!-- editUserId(i.u_id), -->
+                  &emsp;
+            <button v-on:click="deleteUserId(i.u_id), setShow()"> Delete User </button>
+
+          </div>
+
+        </div>
+
+        <div >
+
+          <button @click="formSwitch = !formSwitch" v-show="formSwitch"> New User </button>
+
+  <!-- HERE I HAVE TO CREATE USER DETAIL FORM THAT POPS UP ONLY WHEN CREATE USER IS CLICKED, AND DISPLAYS A FORM THAT ACCEPTS DATA AND CALLS A FUNCTION ON SUBMIT CLICK WITH A JSON FORMAT PARAMETER -->
+  <!-- ADD A SWITCH THAT SHOWS EITHER THE FORM OR THE NEW USER BUTTON -->
+          <div class = "formStyle" v-show="!formSwitch">
+
+              <form>
+                Enter Details: <br>
+
+                <input v-model="userId" type="text" id="id" name="id" placeholder='User Id'><br>
+                <input v-model="firstName" type="text" id="fname" name="fname" placeholder='First Name'><br>
+                <input v-model="middleName" type="text" id="mname" name="mname" placeholder='Middle Name'><br>
+                <input v-model="lastName" type="text" id="lname" name="lname" placeholder='Last name'><br>
+
+                <button @click="newUser(), preventEvent($event), setShow(), formSwitch = !formSwitch"> Create </button>
+                &emsp;
+                <button @click="formSwitch = !formSwitch, preventEvent($event)"> Cancel </button>
+
+              </form>
+
+          </div>
+
+        </div>
+
+
+      </div>
+
+<!-- formSwitchForEdit IS YET TO BE DECLARED, IT IS JUST ANOTHER VARIABLE FOR MAKING EDIT FORM VISIBLE AND INVISIBLE. placeholder='First Name' -->
+
+        <div class = "formStyle" v-show="formSwitchForEdit"> 
+          <!-- A DIV FOR EDIT FORM -->
+          
+          <form> 
+            Edit details: <br> 
+
+            <!-- <input v-model="firstName" type="text" id="fname" name="fname"  value=newVarFirstName><br> -->
+            <input v-model="firstNameForEdit" type="text" id="fname" name="fname"><br>
+            <input v-model="middleNameForEdit" type="text" id="mname" name="mname"><br>
+            <input v-model="lastNameForEdit" type="text" id="lname" name="lname"><br>
+
+            <button @click="closeEditForm(), editUserId(), preventEvent($event), setShow()"> Update </button>
+            &emsp;
+            <button @click="preventEvent($event), closeEditForm()"> Cancel </button>
+
+          </form>
+
+        </div>
           <!-- <div> ADDRESS
 
           </div> -->
@@ -75,11 +119,9 @@
 
             <p> {{reqResponse}} </p>
 
-          </div>
-
-      </div>
-
   </div>
+
+</div>
 
 </template>
 
@@ -93,8 +135,12 @@ export default {
   data() {
     return {
       newVar: [],
+      // newVar1: {},
+      newVarFirstName: '',
       specificUser: [],
       reqResponse: '',
+      userIdForGet: '',
+      formSwitchForEdit: Boolean,
       isShowDelete: Boolean,
       formSwitch: Boolean,
       isShow: Boolean,
@@ -102,6 +148,10 @@ export default {
       firstName: "",
       middleName: "",
       lastName: "",
+      userIdForEdit: "",
+      firstNameForEdit: "",
+      middleNameForEdit: "",
+      lastNameForEdit: "",
     };
   },
 
@@ -109,39 +159,44 @@ export default {
     // this.getUsers();
     this.function();
   },
+  
+    // try {
+    //   const response = await axios.get("http://localhost:5434/v1/users/");
+    //   this.newVar = response.data
+    //   console.log(response.data);
+    // } catch(e) {
+    // } finally {
+    // }
+    // v-show = "formSwitchForEdit"
 
   methods: {
-
-    async getUsers() {
-      // try {
-      //   const response = await axios.get("http://localhost:5434/v1/users/");
-      //   this.newVar = response.data
-      //   console.log(response.data);
-      // } catch(e) {
-      // } finally {
-      // }
-
-      axios.get("http://localhost:5434/v1/users/").then( response =>
-        this.newVar = response.data
-          ).catch ( err => {
-            console.log(err);
-          }
-        )
-    },
-
+    
+    
     function(){
       this.isShow = false;
       this.switch0 = false;
+      this.formSwitch = true;
       this.deleteSwitch = false;
-      this.formSwitch = false;
+      this.formSwitchForEdit = false;
     },
 
-    testGetIdFunction(id) {
-      var url = 'http://localhost:5434/v1/users/' + id
-      axios.get(url). then (response =>
-        this.specificUser = response.data
-      )
+    preventEvent(event) {
+      event.preventDefault();
     },
+
+    showEditForm() {
+      this.formSwitchForEdit = true;
+    },
+    closeEditForm() {
+      this.formSwitchForEdit = false;
+    },
+
+    // testGetIdFunction(id) {
+    //   var url = 'http://localhost:5434/v1/users/' + id
+    //   axios.get(url). then (response =>
+    //     this.specificUser = response.data
+    //   )
+    // },
 
     async deleteUserId (id) {
       var url = 'http://localhost:5434/v1/users/' + id + '/'
@@ -152,6 +207,59 @@ export default {
       this.getUsers();
     },
 
+    async editUserId() {
+
+      var url = 'http://localhost:5434/v1/users/' + this.userIdForEdit + '/'
+      let jsonRes = '{ "first_name":' + '"' + this.firstNameForEdit + '",' + '"middle_name":' + '"' + this.middleNameForEdit + '",' +  '"last_name":' + '"' + this.lastNameForEdit + '" }'
+// console.log(jsonRes);
+      await axios.put(url,jsonRes).then(
+          response =>
+        this.reqResponse = response.data,
+      );
+      this.getUsers();
+    },
+
+    setEditValues(i) {
+      this.userIdForEdit = i.u_id;
+      this.firstNameForEdit = i.first_name;
+      this.middleNameForEdit = i.middle_name;
+      this.lastNameForEdit = i.last_name;
+    },
+
+    async newUser () {
+
+      let url
+      
+      if (this.userId === '' ) {
+        url = 'http://localhost:5434/v1/users/'
+      } else {
+        url = 'http://localhost:5434/v1/users/' + this.userId + '/'
+      }
+
+      let jsonRes = '{ "first_name":' + '"' + this.firstName + '",' + '"middle_name":' + '"' + this.middleName + '",' +  '"last_name":' + '"' + this.lastName + '" }'
+
+      await axios.post(url, jsonRes).then( response =>
+      this.reqResponse = response.data,
+      );
+
+      this.userId = '';
+      this.firstName = '';
+      this.middleName = '';
+      this.lastName = '';
+
+      this.getUsers();
+
+    },
+
+    async getUsers() {
+      await axios.get('http://localhost:5434/v1/users/').then( response =>
+        this.newVar = response.data
+          ).catch ( err => {
+            console.log(err);
+          }
+        )
+    },
+
     setShow () {
       this.isShowDelete = true;
       setTimeout(() => {
@@ -159,25 +267,7 @@ export default {
       }, 1500);
     },
 
-    async newUser () {
-      // console.log(this.firstName, this.middleName, this.lastName);
-      let url
-      if (this.userId === '' ) {
-        url = 'http://localhost:5434/v1/users/'
-      } else {
-        url = 'http://localhost:5434/v1/users/' + this.userId + '/'
-      }
 
-      // let jsonRes = {"first_name": this.firstName, "middle_name": this.middleName, "last_name": this.lastName};
-
-      let jsonRes = '{ "first_name":' + '"' + this.firstName + '",' + '"middle_name":' + '"' + this.middleName + '",' +  '"last_name":' + '"' + this.lastName + '" };'
-
-      console.log(jsonRes);
-      await axios.post(url, jsonRes).then( response =>
-      this.reqResponse = response.data,
-      // console.log(response.data),
-      );
-    },
   },
 };
 
@@ -187,7 +277,7 @@ export default {
 <style>
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Courier New", Courier, monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -201,7 +291,6 @@ export default {
   border: 1px dashed #595959;
   padding: 8px;
   align-items: center;
-  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
 }
 
 .tableId {
@@ -211,23 +300,44 @@ export default {
 
 .tableFirstName {
   display: flex;
-  width: 60%;
+  width: 10%;
+}
+
+.tableMiddleName {
+  display: flex;
+  width: 10%;
+}
+
+.tableLastName {
+  display: flex;
+  width: 50%;
 }
 
 .tableActions {
   display: flex;
-  width: 45%;
+  width: 25%;
 }
 
 .formStyle {
   display: flex;
-  width: 300px;
+  width: 10%;
   font-size: medium;
   margin-bottom: 20px;
   border: 1px dashed #595959;
-  padding: 8px;
-  align-items: left;
-  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  padding: 10px;
+  line-height: 25px;
+}
+
+button{
+  font-family: "Courier New", Courier, monospace;
+}
+
+input{
+    font-family: "Courier New", Courier, monospace;
+}
+
+::placeholder{
+  font-family: "Courier New", Courier, monospace;
 }
 
 </style>
